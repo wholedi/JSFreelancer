2198a0b6045b886fb9466211a6530c509e79ee32
ta.shevchenko <ta.shevchenko@temabit.com>
2023-05-22 19:02:46 +0300

    Did task about object.

---
+++ script_8.js
@@ -0,0 +1,26 @@
+//Задача 1 - правильная запись.
+
+const userInfo = {
+    name: "Вася",
+    age: 30,
+}
+console.log(userInfo);
+
+//Задача 2 - в консоли будет "Значение свойства из-за записи в [].
+
+let userInf = {
+    name: "Вася",
+    age: 30,
+    "58": 'Значение свойства',
+}
+console.log(userInf[58]);
+
+//Задача 3 в консоли будет нвовое значение 45.
+
+let userIn = {
+    name: "Вася",
+    age: 30,
+}
+let user = userInfo;
+user.age = 45;
+console.log(user.age)
