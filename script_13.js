cad3248c78d7e5f1b352686fae0057ac7ab942e0
ta.shevchenko <ta.shevchenko@temabit.com>
2023-05-28 23:22:59 +0300

    Theory part 2.

--- script_13.js
+++ script_13.js
@@ -61,3 +61,67 @@ const elementOffsetParent = block.offsetParent
 1. Для скрытых элеметов (display: none или когда его нет в документе)
 2. Для элементов <body> <html>
 3. Для элементов с position: fixed.*/
+
+//свойства offsetParent, offsetLeft, offsetTop
+
+const elementOffsetLeft = block.offsetLeft
+const elementOffsetTop = block.offsetTop
+
+//получаем размеры объекта
+
+const elementOffsetWidth = block.offsetWidth
+const elementOffsetHeight = block.offsetHeight
+
+//отсупы внутренней части элемента от внешней clientTop clientLeft
+
+const elementClientTop = block.clientTop
+const elementClientLeft = block.clientLeft
+
+//размеры объекта без рамок и полосы прокрутки clientWidth clientHeight
+
+const elementClientWidth = block.clientWidth
+const elementClientHeight = block.clientHeight
+
+//размеры объекта включающие в себя прокрученную (которую не видно) часть scrollWidth scrollHeight
+
+const elementScrollWidth = block.scrollWidth
+const elementScrollHeight = block.scrollHeight
+
+//размеры прокрученной области scrollLeft scrollTop
+
+const elementScrollLeft = block.scrollLeft
+const elementScrollTop = block.scrollTop
+
+//--------------------------------------------Координаты-------------------------------------------------
+/*Большинство работают в системах координат:
+1. Относительно окна браузера clientX clientY getBoundingClientRect
+2. Относительно документа pageX pageY
+
+Если происходит прокрутка, то координаты элементов в контексте окна меняются,
+координаты относительно документа остаются такими же*/
+
+//получаем объект
+
+const item = document.querySelector('.lesson__item')
+
+//получаем координаты относительно окна браузера
+
+const getItemCoords = item.getBoundingClientRect()
+
+//получаем конкретную координату относительно ОКНА браузера
+
+const getItemLeft = item.getBoundingClientRect().left
+
+ //получаем конкрутную координату относительно ДОКУМЕНТА
+
+//const getItemTopDocumentCoords = getItemTopCoords + window.pageYOffset
+
+//получаем объект по координатам document.elementFromPoint(x, y)
+
+const elem = document.elementFromPoint(100, 100) //данные координаты относительно окна браузера
+
+
+
+
+
+
